3fe471a8d2b7c7f88efd741bb621aa097edf8e65
Simon Egersand <s.egersand@gmail.com>
2017-03-07 23:52:47 +0100

    Ruby day 1

---
+++ ruby/tree.rb
@@ -0,0 +1,29 @@
+class Tree
+      attr_accessor :children, :node_name
+
+      def initialize(name, children=[])
+          @children = children
+          @node_name = name
+      end
+
+      def visit_all(&block)
+          visit &block
+          children.each {|c| c.visit_all &block}
+      end
+
+      def visit(&block)
+          block.call self
+      end
+end
+
+tree = Tree.new( "Ruby",
+                [Tree.new("Reia"),
+                 Tree.new("MacRuby")] )
+
+puts "Visiting a node"
+tree.visit {|node| puts node.node_name}
+puts
+
+puts "visiting entire tree"
+tree.visit_all {|node| puts node.node_name}
+
